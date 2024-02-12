810dc3c6275b05fea5329cc58d7bebae7c9a00e2
HunterOBrien <obrienh2@middleton.school.nz>
2024-02-13 12:12:09 +1300

    red square on screen

--- snake_01.py
+++ snake_01.py
@@ -1,5 +1,36 @@
 import pygame
+import time
+
 pygame.init()
+
+game_icon = pygame.image.load('images/snake_icon.png')
+pygame.display.set_icon(game_icon)
+pygame.display.set_caption("Snake game - by Patrick Baker")
+screen = pygame.display.set_mode((1000, 720))
+
+# Colour hex codes
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (188, 227, 199)
+
+# Game Fonts
+score_font = pygame.font.SysFont("arialblack", 20)
+exit_font = pygame.font.SysFont("freesansbold.ttf", 30)
+
+# snake is 20 x 20 pixels at start
+snake_x = 490  # (1000 -20) /2  snake is 20pixels so taken away before finding middle value
+snake_y = 350  # (720 -20) /2  snake is 20pixels so taken away before finding middle value
+
+quit_game = False
+while not quit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit_game = True
+
+    pygame.draw.rect(screen, red, [snake_x, snake_y, 20, 20])
+    pygame.display.update()
+
 
 
 pygame.quit()
